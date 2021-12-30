afe34619bad806440cf0587e3b112acbebd939ad
Abdullah Al Noman <kazinoman9999@gmail.com>
2021-12-30 12:23:05 +0600

    modify arraw fun again

--- arrow.js
+++ arrow.js
@@ -5,3 +5,10 @@ fun();
 
 const function2 = (a, b) => a + b;
 console.log(function2(2, 3));
+
+//param => expression       ||one parameter and single exression no need to parenthisys and return.
+
+function func() {
+  return arguments;
+}
+func("fun");
